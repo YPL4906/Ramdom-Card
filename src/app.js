af37488ffd4ce91030f4a7ed6df88275f29d4e59
YPL4906 <104123424+YPL4906@users.noreply.github.com>
2024-11-29 11:26:51 +0000

    Version Final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //arrays de la carta
+  let baraja = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let palos = ["♠", "♣", "♥", "♦"];
+  //Funcion que genera numero aleatorio
+  function generarNumero() {
+    let barajaIndex = Math.floor(Math.random() * baraja.length);
+    return `${baraja[barajaIndex]}`;
+  }
+  //Funcion que genera palo aleatorio
+  function generarPalo() {
+    let palosIndex = Math.floor(Math.random() * palos.length);
+    return `${palos[palosIndex]}`;
+  }
+  //Creo una constante palo de la funcion anterior para trabajar individualmente en el HMTL y CSS
+  const palo = generarPalo();
+  document.getElementById("palo-superior").innerText = palo;
+  document.getElementById("palo-inferior").innerText = palo;
+
+  document.getElementById("numero").textContent = generarNumero();
+
+  //seleccion del div carta
+  const carta = document.querySelector(".carta");
+  const paloSuperior = document.querySelector(".palo.superior");
+  const paloInferior = document.querySelector(".palo.inferior");
+
+  // Modificaciones en el con el DOM
+  carta.setAttribute("data-palo", palo);
+  paloSuperior.innerText = palo;
+  paloInferior.innerText = palo;
 };
